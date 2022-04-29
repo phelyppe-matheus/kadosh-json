398693cb5573c3a47fd880b1eea2d280d753b6bc
PhePheMath <PhelyppeMath@outlook.com>
2022-04-29 12:26:09 -0300

    returning all posts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const fs = require("fs")
+
+let raw_data = fs.readFileSync('initial_data.json');
+let posts = JSON.parse(raw_data);
+
 const app = express();
 const PORT = 5000;
 
-app.get('/', function(req, res) {
-    res.send("WORKING")
+app.get('/', function (req, res) {
+    res.json(posts);
 });
 
-app.listen(process.env.PORT || PORT);
+app.listen(process.env.PORT || PORT, () => console.log(`Listening from port ${PORT}`));
