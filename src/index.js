e507ece9024d1df7cac162f4870b3dac89ef2402
PhePheMath <PhelyppeMath@outlook.com>
2022-04-29 01:14:48 -0300

    (fix) swap req and res on path '/'

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
-app.get('/', function(res, req) {
-    req.send("WORKING")
+app.get('/', function(req, res) {
+    res.send("WORKING")
 });
 
 app.listen(process.env.PORT || PORT);
